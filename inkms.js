a779ac315b47d18a363f13faffbb4ce360dcdfc7
Ron-Raz <ron.raz@kaltura.com>
2019-08-01 09:23:09 -0400

    aug01

--- inkms.js
+++ inkms.js
@@ -1,15 +1,27 @@
 console.log("in inkms.js");
 var pathname = window.location.pathname;
+var mytimerhandle;
 $(function () {
     console.log("doc ready, pathname=", pathname);
     if (!pathname.startsWith('/media/')) return;
     console.log("media entry. setting timer");
-    window.setInterval(mytimer, 100);
+    mytimerhandle = window.setInterval(mytimer, 100);
 });
-var it = 0;
+var it = 15;
 function mytimer() {
     var mt = $("#entry-metadata > dd > div > div").text();
-    var mh = $("#entry-metadata").html();
-    it += 1;
-    console.log("it=", it, " mt=", mt, " mh=", mh);
+    it -= 1;
+    // stop conditions: found mt | timeout
+    if (mt.length > 2 || it === 0) {
+        console.log("it=", it, "mt=", mt);
+        clearInterval(mytimerhandle);
+        if (mt.length > 2) {
+            embedSlido();
+        }
+    }
+}
+
+function embedSlido() {
+    console.log("embedSlido")
+    $("#mySidebar").hide();
 }
