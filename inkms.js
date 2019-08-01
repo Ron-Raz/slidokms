7b8ae2ff0667c3dec582896a8eaaf20df3d5b628
Ron-Raz <ron.raz@kaltura.com>
2019-08-01 06:08:22 -0400

    ver2

--- inkms.js
+++ inkms.js
@@ -1,1 +1,7 @@
-console.log("in inkms.js 4:34");
+console.log("in inkms.js ver2");
+var pathname= window.location.pathname;
+if( pathname.startsWith('/media/')) { // check if a media entry
+    console.log("media entry");
+} else {
+    console.log("do nothing")
+}
